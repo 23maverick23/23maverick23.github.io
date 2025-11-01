5d0d6ab4d4737500aba5629067badbac418233ed
Ryan Morrissey <23maverick23@gmail.com>
2025-10-31 20:10:38 -0600

    Optimize Astro site (simplify)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://ryancmorrissey.com',
-    base: '/',
+	site: "https://ryancmorrissey.com",
+	base: "/",
+	output: "static",
+	integrations: [sitemap()],
 });
